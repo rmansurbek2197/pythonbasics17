1e8ae00d3aca3597e793bd472f60157285a348b6
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:31:14 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-N = int(input())
-for i in range(1, N + 1):
-    if i % 2 != 0:
-        print(i)
+def remove_duplicates(input_list):
+    output_list = []
+    for element in input_list:
+        if element not in output_list:
+            output_list.append(element)
+    return output_list
+
+def main():
+    input_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9]
+    print("Original list: ", input_list)
+    output_list = remove_duplicates(input_list)
+    print("List after removing duplicates: ", output_list)
+
+main()
